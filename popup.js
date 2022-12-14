a47fdf6c7ee7e2d8606b75175101f2e6361faa3f
e-labinnovations <mohammedashad23@gmail.com>
2022-12-14 23:08:28 +0530

    Basic popup working

---
+++ popup.js
@@ -0,0 +1,18 @@
+const daysElement = document.getElementById('days')
+
+chrome.storage.sync.get(['text-color', 'start-date'], result => {
+    if (result['start-date'] && result['text-color']) {
+        var startDate = result['start-date'];
+        var splitter = (startDate.indexOf("/") == -1) ? "-" : "/";
+        var parts = startDate.split(splitter);
+        //calculate elapsed days
+        var oneDay = 24*60*60*1000; // hours*minutes*seconds*milliseconds
+        var firstDate = new Date(parts[0],parts[1]-1,parts[2]);
+        var secondDate = new Date();
+        var diffDays = Math.floor((secondDate.getTime() - firstDate.getTime())/(oneDay));
+        var daySize = (Math.abs(diffDays) < 10) ? 14 : (Math.abs(diffDays) < 100) ? 12 : (Math.abs(diffDays) < 1000) ? 10 : (Math.abs(diffDays) < 10000) ? 8 : 7;
+        daysElement.innerText = diffDays + ' Days'
+    } else {
+        daysElement.innerText = 'Zero Days'
+    }
+});
