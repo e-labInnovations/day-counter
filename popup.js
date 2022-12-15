65d1613191d2d3ef8e71fcd126b7b469939ea817
e-labinnovations <mohammedashad23@gmail.com>
2022-12-15 21:22:27 +0530

    Popup page updated

--- popup.js
+++ popup.js
@@ -11,8 +11,16 @@ chrome.storage.sync.get(['text-color', 'start-date'], result => {
         var secondDate = new Date();
         var diffDays = Math.floor((secondDate.getTime() - firstDate.getTime())/(oneDay));
         var daySize = (Math.abs(diffDays) < 10) ? 14 : (Math.abs(diffDays) < 100) ? 12 : (Math.abs(diffDays) < 1000) ? 10 : (Math.abs(diffDays) < 10000) ? 8 : 7;
-        daysElement.innerText = diffDays + ' Days'
+        daysElement.innerText = diffDays
     } else {
-        daysElement.innerText = 'Zero Days'
+        daysElement.innerText = '0'
     }
 });
+
+document.querySelector('#go-to-options').addEventListener('click', function() {
+    if (chrome.runtime.openOptionsPage) {
+      chrome.runtime.openOptionsPage();
+    } else {
+      window.open(chrome.runtime.getURL('options.html'));
+    }
+  });
